6dd38df7bdc061cef7b0c1e76e5edc48a58ccb64
ankitshahu <ankit@matchlog.delivery>
2022-09-08 20:43:08 +0530

    added webhook

--- controllers/message.js
+++ controllers/message.js
@@ -22,8 +22,6 @@ exports.webhook = async (req, res) => {
 exports.whatsappmessage = async (req, res) => {
   let body_param = req.body;
 
-  console.log(JSON.stringify(body_param, null, 2));
-
   if (body_param.object) {
     console.log("inside body param");
     if (
@@ -35,7 +33,7 @@ exports.whatsappmessage = async (req, res) => {
       let phon_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      let msg_body = body_param.entry[0].changes[0].value.messages[0];
 
       console.log("phone number " + phon_no_id);
       console.log("from " + from);
